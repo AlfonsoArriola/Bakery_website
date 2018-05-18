f23b028ea8aa755177f7527ba65f75f02b4d133e
Alfonso Arriola <alfonsoarriolajr@gmail.com>
2018-05-18 17:53:18 -0400

    new commit

---
+++ public/main.js
@@ -0,0 +1,15 @@
+    document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, options);
+  });
+
+
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+      direction: 'left',
+      hoverEnabled: false
+    });
+  });
--- server.rb
+++ server.rb
@@ -1,19 +1,62 @@
+
 require "sinatra"
+
+# class Pastry  
+# 	def initialize(description, item)
+# 		@description = description
+# 		@item = item
+# 	end
+# end	
+
+class Cake
+	def initialize(description, item)
+		@description = description
+		@item = item
+	end
+end
+
+class Cookies
+	def initialize(description,item)
+	    @description = description
+		@item = item	
+	end
+end
+
+class Muffins
+	def initialize(description, item)
+		@description = description
+		@item = item
+	end
+end
+
+@peanut_butter_cookie = Cookies.new("Peanut Butter",2)
+
+
 
 get('/')  do
 	"I'm alive!"
+
 	erb :index
 end
 
 
 get('/cakes')  do
 	"I'm cakes!"
+	p @peanut_butter_cookie
+
+
 	
 end
 
 
 get('/cookies')  do
-	"I'm cookies!"
+	# "I'm cookies!"
+
+	# @peanut_butter_cookie
+
+	# "I'm a #{@peanut_butter_cookie}"
+
+	"I'm a #{@peanut_butter_cookie}"
 	
 end
 
